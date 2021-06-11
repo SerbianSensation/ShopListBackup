2f0870b530de35b09723a883dffe50d3d8178928
Stefan Gligorevic <stefangligorevic@gmail.com>
2021-06-11 09:33:32 -0400

    Added search bar on the cart page to search both complete and incomplete items. To do: make search bars not case sensitive. Maybe also have only one search bar on carts page.

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -3,9 +3,12 @@
     <h2 class="pad" v-if="!cart.length">Shopping Cart is empty</h2>
     <div v-else>
       <h2 class="pad">Shopping Cart</h2>
+      <!-- search bar !-->
+      <v-icon>search</v-icon>
+      <input type="text" v-model="searchIncomplete" placeholder="Search Incomplete Items" /> <br>
       <!-- loop through non-completed items first !-->
       <h3 class="text-left">Incomplete Items</h3>
-      <v-list-item v-for="item in nonCompletedItems" :key="item.id" @click="{}">
+      <v-list-item v-for="item in filterIncompleteItems" :key="item.id" @click="{}">
         <!-- Item contents !-->
         <v-list-item-content>{{ item.name }}</v-list-item-content>
         <v-list-item-content>Complete: {{ item.complete }}</v-list-item-content>
@@ -28,9 +31,12 @@
         </v-list-item-action>
       </v-list-item>
 
+      <!-- search bar !-->
+      <v-icon>search</v-icon>
+      <input type="text" v-model="searchComplete" placeholder="Search Complete Items" /> <br>
       <!-- loop through completed items first !-->
       <h3 class="text-left">Completed Items</h3>
-      <v-list-item v-for="item in completedItems" :key="item.id" @click="{}">
+      <v-list-item v-for="item in filterCompleteItems" :key="item.id" @click="{}">
         <!-- Item contents !-->
         <v-list-item-content>{{ item.name }}</v-list-item-content>
         <v-list-item-content>Complete: {{ item.complete }}</v-list-item-content>
@@ -63,9 +69,28 @@
 import { mapActions, mapState, mapGetters } from 'vuex';
 
 export default {
+  data() {
+    return {
+      searchComplete: '',
+      searchIncomplete: ''
+    };
+  },
   computed: {
     ...mapState(['cart']),
-    ...mapGetters(['completedItems', 'nonCompletedItems'])
+    ...mapGetters(['completedItems', 'nonCompletedItems']),
+    //filter complete and incomplete items for search bar
+    filterCompleteItems: function () {
+      //return items matching the current filter in search bar
+      return this.completedItems.filter((item) => {
+        return item.name.match(this.searchComplete);
+      })
+    },
+    filterIncompleteItems: function () {
+      //return items matching the current filter in search bar
+      return this.nonCompletedItems.filter((item) => {
+        return item.name.match(this.searchIncomplete);
+      })
+    }
   },
   methods: {
     ...mapActions(["removeFromCart", "updateComplete", "updateCurrentItem", "clearCart", "incOrder"])
